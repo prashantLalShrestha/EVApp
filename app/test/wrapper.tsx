407c0524c82dfaddf1fb5b97539182a08d8e4271
Prashant Shrestha <prashant.shrestha@elements.nl>
2024-04-23 13:04:49 +0200

    fix: wrapper component update

--- app/test/wrapper.tsx
+++ app/test/wrapper.tsx
@@ -1,16 +1,13 @@
 import React, { FC } from 'react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { NavigationContainer } from '@react-navigation/native'
-import { Provider } from 'react-redux'
 
-import { store } from '@/store'
 import { ThemeProvider } from '@/theme'
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
       retry: false,
-      cacheTime: 0,
     },
   },
 })
@@ -20,11 +17,9 @@ type Props = {
 }
 
 export const Wrapper: FC<Props> = ({ children }) => (
-  <Provider store={store}>
-    <QueryClientProvider client={queryClient}>
-      <ThemeProvider theme="light">
-        <NavigationContainer>{children}</NavigationContainer>
-      </ThemeProvider>
-    </QueryClientProvider>
-  </Provider>
+  <QueryClientProvider client={queryClient}>
+    <ThemeProvider>
+      <NavigationContainer>{children}</NavigationContainer>
+    </ThemeProvider>
+  </QueryClientProvider>
 )
